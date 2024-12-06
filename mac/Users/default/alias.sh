e9f3de369bbcfc6b1a591c393cd9c59a9cbf703a
SeokYoung Kim <crong@mini-2.local>
2024-12-06 18:32:26 +0900

    feat: add macos

---
+++ mac/Users/default/alias.sh
@@ -0,0 +1,5 @@
+alias ls='ls --color=auto'
+alias ll='lsd -alhFv --group-directories-first'
+alias grep='grep --color=auto'
+alias vi='vim'
+alias tf='terraform'
---
+++ mac/Users/default/path.sh
@@ -0,0 +1,8 @@
+PATH="/opt/homebrew/opt/curl/bin:$PATH"
+
+export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
+export LDFLAGS="-L/opt/homebrew/opt/curl/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
+export TIME_STYLE='long-iso'
+export EDITOR='vim'
+export PATH
