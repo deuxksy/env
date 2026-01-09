76430770abb49d1a8abcb84800a657583b2bf72a
Crong <deuxksy@gmail.com>
2026-01-09 21:07:43 +0700

    feat: 미사용 삭제

--- bash/dns.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-IP=$(curl -s ifconfig.co)
-curl -s "https://<USERNAME>:<PASSWORD>@domains.google.com/nic/update?hostname=<SUBDOMAIN>.zzizily.com&myip=${IP}"
--- bash/restart_glances_web.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pkill -f "glances -w"
-
-
-nohup glances -w &
